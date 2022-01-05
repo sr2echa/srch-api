c04ab493d782c0386d77d4eb19c206be688056f1
Sreecharan <sr33ch4@yahoo.com>
2022-01-05 05:57:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,7 @@ def home():
     #return "<h1>SRCH</h1><p>Supercharge your web searches</p>"
     return render_template('index.html')
     
-app.run(host='127.0.0.1')
+if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
+        #app.run()
